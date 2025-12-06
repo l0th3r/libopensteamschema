4e74e91689fbe847daddd5515806577a116fc3af
l0th3r <58365407+l0th3r@users.noreply.github.com>
2025-12-06 14:39:25 -0500

    implement coverage report generation

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,14 @@ set -e
 BUILD_TYPE=Debug
 BUILD_DIR=.build
 CMAKE_FLAGS=()
+OPT_FLAGS=()
 
 # Doc
 # DOC_REQUESTED=false
 
 # Test and coverage
 TEST_REQUESTED=false
-# COVR_REQUESTED=false
-# COVR_EXE_REQUESTED=false
+COVR_REQUESTED=false
 
 for arg in "$@";
   do
@@ -22,16 +22,31 @@ for arg in "$@";
       CMAKE_FLAGS+=("-DOPENITEMDEF_BUILD_TESTS=ON")
     elif [ "$arg" == "-release" ]; then
       BUILD_TYPE=Release
+    elif [ "$arg" == "-cov" ]; then
+      CMAKE_FLAGS+=("-DOPENITEMDEF_ENABLE_COVERAGE=ON")
+      CMAKE_FLAGS+=("-DOPENITEMDEF_BUILD_TESTS=ON")
+      TEST_REQUESTED=true
+      COVR_REQUESTED=true
+    else
+      OPT_FLAGS+="$arg"
     fi
 done
 
 echo ">> Building lib"
 
-cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE "${CMAKE_FLAGS[@]}"
+cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE "${CMAKE_FLAGS[@]}" "${OPT_FLAGS[@]}"
 cmake --build "$BUILD_DIR" --config $BUILD_TYPE
 
 if [ "$TEST_REQUESTED" = true ]; then
   echo
   echo ">> Testing lib"
   ctest --test-dir "$BUILD_DIR" --output-on-failure
+fi
+
+if [ "$COVR_REQUESTED" = true ]; then
+  echo ">> Capturing coverage..."
+  lcov --directory "$BUILD_DIR" --capture --output-file "$BUILD_DIR/coverage.info"
+  lcov --remove "$BUILD_DIR/coverage.info" '/usr/*' '*/tests/*' -o "$BUILD_DIR/coverage.cleaned.info"
+  genhtml "$BUILD_DIR/coverage.cleaned.info" --output-directory "$BUILD_DIR/coverage-report" --legend --title "libopenmodelviewer Coverage"
+  echo ">> Coverage report available in: $BUILD_DIR/coverage-report/index.html"
 fi
